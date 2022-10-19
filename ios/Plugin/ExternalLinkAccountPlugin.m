9ee0bf22b1e422ca248ae586d0ba9cfedd24c034
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-10-19 13:39:26 +0200

    🔧 Register plugin methods for ios

--- ios/Plugin/ExternalLinkAccountPlugin.m
+++ ios/Plugin/ExternalLinkAccountPlugin.m
@@ -4,5 +4,6 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(ExternalLinkAccountPlugin, "ExternalLinkAccount",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(canOpen, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(open, CAPPluginReturnPromise);
 )
